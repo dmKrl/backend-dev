4b7e449b5948bff13af00ad3564999320c373e01
Dmitry Kralichkin <dm_krl@mail.ru>
2023-12-08 13:16:36 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const getUsers = require('./modules/users');
+
+const server = http.createServer((request, response) => {
+  if (request.url === '/users') {
+    response.status = 200;
+    response.statusMessage = 'OK';
+    response.header = 'Content-Type: application/json';
+    response.write(getUsers());
+    response.end();
+    return;
+  }
+  if (request.url === '/users') {
+    response.status = 200;
+    response.statusMessage = 'OK';
+    response.header = 'Content-Type: application/json';
+    response.write(getUsers());
+    response.end();
+    return;
+  }
+
+  response.status = 200;
+  response.statusMessage = 'OK';
+  response.header = 'Content-Type: text/plain';
+  response.write('Hello, world!');
+  response.end();
+});
+
+server.listen(3003, () => {
+  console.log('Сервера запущен по адресу http://127.0.0.1:3003');
+});
