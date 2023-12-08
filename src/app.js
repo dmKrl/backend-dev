b4ce871c0465821f8ddb12c69d4499f9d7f4b9ab
Dmitry Kralichkin <dm_krl@mail.ru>
2023-12-08 17:05:05 +0700

    ready hw

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const http = require('http');
 const getUsers = require('./modules/users');
 
 const server = http.createServer((request, response) => {
+  const url = new URL(request.url, 'http://127.0.0.1');
   if (request.url === '/users') {
     response.status = 200;
     response.statusMessage = 'OK';
@@ -10,15 +11,27 @@ const server = http.createServer((request, response) => {
     response.end();
     return;
   }
-  if (request.url === '/users') {
-    response.status = 200;
+  if (url.searchParams.get('hello') === '') {
+    response.status = 400;
     response.statusMessage = 'OK';
-    response.header = 'Content-Type: application/json';
-    response.write(getUsers());
+    response.header = 'Content-Type: text/html';
+    response.write(`Enter a name`);
     response.end();
     return;
   }
-
+  if (url.searchParams.get('hello') !== null) {
+    response.status = 200;
+    response.statusMessage = 'OK';
+    response.header = 'Content-Type: text/html';
+    response.write(`Hello ${url.searchParams.get('hello')}.`);
+    response.end();
+    return;
+  }
+  if (!url.searchParams.has('hello') && request.url !== '/') {
+    response.status = 500;
+    response.end();
+    return;
+  }
   response.status = 200;
   response.statusMessage = 'OK';
   response.header = 'Content-Type: text/plain';
@@ -26,6 +39,7 @@ const server = http.createServer((request, response) => {
   response.end();
 });
 
-server.listen(3003, () => {
-  console.log('Сервера запущен по адресу http://127.0.0.1:3003');
+const port = process.env.PORT || 3003;
+server.listen(port, () => {
+  console.log(`Сервера запущен по адресу http://127.0.0.1 с портом ${port}`);
 });
